74af3cd94c5b185b0b1a3138442c3e3cbd7c705b
parth <paaedox0@gmail.com>
2025-02-22 15:06:39 +0530

    updated scrolling and modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import LoginSignup from "./components/LoginSignup";
 
 const App = () => {
+  const projectsRef = useRef(null);
+  const [isLoginSignupOpen, setIsLoginSignupOpen] = useState(false);
+
+  const scrollToProjects = () => {
+    projectsRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const openLoginSignup = () => {
+    setIsLoginSignupOpen(true);
+  };
+
+  const closeLoginSignup = () => {
+    setIsLoginSignupOpen(false);
+  };
+
   return (
-    <>
-      <Navbar />
+    <div className="min-h-screen flex flex-col">
+      <Navbar openLoginSignup={openLoginSignup} />
       <Hero />
-      <Projects />
-      <Footer />
-    </>
+      <Projects ref={projectsRef} />
+      <Footer scrollToProjects={scrollToProjects} />
+      <LoginSignup isOpen={isLoginSignupOpen} onClose={closeLoginSignup} />
+    </div>
   );
 };
 
